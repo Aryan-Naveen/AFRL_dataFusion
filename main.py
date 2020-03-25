aa21ce143ec90be75fc15a8f09c704ad7e87382c
Aryan <aryanpn@gmail.com>
2020-03-25 13:58:26 -0400

    Added code to label network

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ class Space():
         self.size_box = 0
         self.border = 0
 
+    def get_figure(self):
+        return plt.figure()
+    
+    def get_axes(self):
+        if(self.dim >= 3):
+            return plt.axes(projection='3d')
+        else:
+            return plt.axes()
+
+
 def common_rows(matrix, ind):
     occur = []
     for i in range(len(matrix)):
@@ -27,19 +37,16 @@ P_link = .02
 axis_length = 100
 
 my_space.ang_meas_sigma = 5 * math.pi/180
-my_space.dim = 3
+my_space.dim = 2
 
-#Not sure what these are for
 my_space.size_box = axis_length * np.ones((my_space.dim,1))
 my_space.border = 10
 
 
-#Not sure what this is representing
 target_loc = np.random.rand(my_space.dim, 1) * (my_space.size_box - 2*my_space.border) + my_space.border
 
 A, sens = generateGraphHypercube(my_space.dim, N_agents, 0.2)
 
-#What is axis length
 sens *= axis_length
 
 
@@ -47,7 +54,6 @@ curr_A_pow = np.copy(A)
 sum_A = np.copy(A)
 dist = 1
 
-#Which is the graph A or sum_A also what is this loop doing?
 while np.count_nonzero(sum_A == 0) > 0:
     curr_A_pow = np.dot(curr_A_pow, A)
     sum_A += curr_A_pow
@@ -62,30 +68,11 @@ for i in range(len(neighbors)):
     neighbors[i] = np.nonzero(A[i])
 neighbors = np.array(neighbors)
 
-if my_space.dim >= 3:
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(target_loc[0], target_loc[1], target_loc[2])
-else:
-    plt.plot(target_loc[0], target_loc[1])
+fig = my_space.get_figure()
+ax= my_space.get_axes()
 
-plt.show()
-
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-for i in range(N_agents):
-    if D[i] > 0:
-        if my_space.dim > 2:
-            ax.scatter3D(sens[0][i], sens[1][i], sens[2][i])
-        else:
-            plt.plot(sens[0][i], sens[1][1])
-plt.show()
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
 for i in range(1, N_agents):
-    if D[i] > 0:
+    if D[i] >= 0:
         for j in range(1, i):
             if A[i][j] == 1:
                 if my_space.dim > 2:
@@ -96,5 +83,15 @@ for i in range(1, N_agents):
                 else:
                     X = [sens[0][i], sens[0][j]]
                     Y = [sens[1][i], sens[1][j]]
-                    plt.plot(X, Y)
+                    ax.plot(X, Y)
+
+if my_space.dim >= 3:
+    ax.scatter3D(target_loc[0], target_loc[1], target_loc[2])
+else:
+    ax.scatter(target_loc[0], target_loc[1])
+
+plt.title("Sensor network")
+plt.ylabel("Distance (m)")
+plt.xlabel("Distance (m)")
+
 plt.show()                    

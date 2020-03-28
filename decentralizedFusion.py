Fixed covariance matrix bug

decentralizedFusion.py
@@ -5,6 +5,26 @@ import math
 import random
 from scipy.stats import multivariate_normal
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+def plot_ellipse(covariance, ax, label_t):
+    x_el = np.array([np.sin(np.linspace(0, 2*math.pi, num=63)), np.cos(np.linspace(0, 2*math.pi, num=63))])
+    C = np.linalg.cholesky(covariance)
+    y_el = np.dot(C, x_el)
+    ax.plot(y_el[0], y_el[1], label=label_t)
+
+
+def CI(mu_s, cov_s):
+    w = 1./len(mu_s)
+    total_cov = np.zeros(cov_s[0].shape)
+    total_mu = np.zeros(mu_s[0].shape)
+    for mu, cov in zip(mu_s, cov_s):
+        total_mu = np.add(total_mu, w*np.dot(lin.inv(cov), mu))
+        total_cov = np.add(total_cov, w*lin.inv(cov))
+    
+    comb_cov = lin.inv(total_cov)
+    mu_comb = np.dot(comb_cov, total_mu)
+    return mu_comb, comb_cov
 
 def CI_update(mu_a, cov_a, mu_b, cov_b, w):
     cov_comb = lin.inv(np.add(w*np.linalg.inv(cov_a), (1-w)*lin.inv(cov_b)))
@@ -13,23 +33,38 @@ def CI_update(mu_a, cov_a, mu_b, cov_b, w):
     mu_comb = np.dot(cov_comb, (np.add(sens_1, sens_2)))
     return mu_comb, cov_comb
 
+def get_weights(sensor_covariances):
+    determinats = np.array([np.linalg.det(cov) for cov in sensor_covariances])
+    c = np.copy(determinats)
+    m_det = max(determinats)
+    determinats = np.array([m_det - det for det in determinats])
+    weights = determinats/np.linalg.norm(determinats)
+    return weights, c
+
 def covarianceIntersection(sensor_readings, sensor_covariances, N_time_steps, neighbors, N_agents, KL_inp, true_dist):
     KL_div = {}
+    ax = plt.axes()
+    determinant_progression = {}
     for i in range(N_agents):
         KL_div[i] = []
+        determinant_progression[i] = [lin.det(sensor_covariances[i])]
     print("Covariance Intersection...")
     for i in tqdm(range(N_time_steps)):
         for j in range(N_agents):
-            sensor_mu = sensor_readings[j]
-            sensor_cov = sensor_covariances[j]
+            mu_s = [sensor_readings[j]]
+            cov_s = [sensor_covariances[j]]
+            if i==0:
+                plot_ellipse(sensor_covariances[j], ax, "Sensor " + str(j))
             for k in neighbors[j][0]:
-                mu_k = sensor_readings[k]
-                cov_k = sensor_covariances[k]
-                sensor_mu, sensor_cov = CI_update(sensor_mu, sensor_cov, mu_k, cov_k, 0.5)
-
-            sensor_readings[j] = sensor_mu
-            sensor_covariances[j] = sensor_cov
-            dist = multivariate_normal(sensor_mu, sensor_cov)
+                mu_s.append(sensor_readings[k])
+                cov_s.append(sensor_covariances[k])
+            sensor_readings[j], sensor_covariances[j] = CI(mu_s, cov_s)
+            dist = multivariate_normal(sensor_readings[j], sensor_covariances[j])
             KL_div[j].append(compute_KL(true_dist, dist, KL_inp))
-
-    return sensor_readings, sensor_covariances, KL_div
+            determinant_progression[j].append(lin.det(sensor_covariances[j]))
+    plot_ellipse(sensor_covariances[0], ax, "FINAL")
+    plt.legend(loc='upper left', borderaxespad=0.)
+    plt.grid(b = True)
+    plt.title("Covariance Ellipses")
+    plt.show()
+    return sensor_readings, sensor_covariances, KL_div, determinant_progression

gaussianDistribution.py
@@ -8,7 +8,7 @@ def sampleMeasuredSensorFromTrue(dims, num_sensors, target_loc):
     mu = toTuple(target_loc)
     for i in range(num_sensors):
         S = np.tril(np.random.rand(dims, dims))
-        cov = S*S.T
+        cov = np.dot(S, S.T)
         covs.append(cov)
         mu_i = np.random.multivariate_normal(mu, cov, 1)[0]
         sensor_mus.append(mu_i)

main.py
@@ -47,7 +47,7 @@ N_max_gmms = 15
 P_link = .02
 axis_length = 100
 KL_inputs = []
-N_time_steps = 100
+N_time_steps = 50
 
 #Used to plot pdf functions
 
@@ -63,7 +63,7 @@ target_loc = (np.random.rand(my_space.dim, 1) * (my_space.size_box - 2*my_space.
 for val in target_loc:
     #For computation time 
     if(my_space.dim < 3):
-        KL_inputs.append(np.linspace(val-5, val+5,num=50))
+        KL_inputs.append(np.linspace(val-5, val+5,num=30))
     else:
         KL_inputs.append(np.linspace(val-2, val+2,num=10))
 KL_inputs = np.array(KL_inputs)
@@ -133,11 +133,31 @@ P_centralized = multivariate_normal(fused_mu, fused_cov)
 
 #CI Decentralized Fusion
 
-sensor_mus, sensor_covs, KL_div = covarianceIntersection(sensor_mus, sensor_covs, N_time_steps, neighbors, N_agents, KL_inputs, P_centralized)
+sensor_mus, sensor_covs, KL_div, determinants = covarianceIntersection(sensor_mus, sensor_covs, N_time_steps, neighbors, N_agents, KL_inputs, P_centralized)
 ax = plt.axes()
 X_axis = [i for i in range(1, N_time_steps + 1)]
 for i in range(N_agents):
     kl = KL_div[i]
-    ax.plot(X_axis, kl)
+    ax.plot(X_axis, kl, label="Sensor " + str(i + 1))
 
+plt.ylabel("KL Divergence")
+plt.xlabel("Time Steps")
+plt.title("KL Divergence Progression")
+plt.legend(loc='upper left', borderaxespad=0.)
+plt.grid(b = True)
 plt.show()
+
+ax = plt.axes()
+for i in range(N_agents):
+    deter = determinants[i][1:]
+    ax.plot(X_axis, deter, label = "Sensor " + str(i + 1))
+
+plt.ylabel("Determinant of Covariance Matrix")
+plt.xlabel("Time Steps")
+plt.title("Covariance Progression")
+plt.legend(loc='upper left', borderaxespad=0.)
+plt.grid(b = True)
+plt.show()
+
+
+
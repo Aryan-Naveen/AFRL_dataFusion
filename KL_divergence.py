Fixed KL computation error also runs much faster now

KL_divergence.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from tqdm import tqdm
 
 np.seterr(divide='ignore', invalid='ignore')
 
@@ -31,15 +32,4 @@ def compute_KL(P, Q, input):
     ignored_states = np.seterr(**old_err_state)
     p = np.array(p)
     q = np.array(q)
-    div = np.divide(p, q)
-    fixed_div = []
-    fixed_p = []
-    for j in range(len(div)):
-        i = div[j]
-        if not(i == float('-inf') or i == float('+inf') or math.isnan(i) or abs(i) < 1e-200):
-            fixed_div.append(i)
-            fixed_p.append(p[j])
-    fixed_div = np.array(fixed_div)
-    fixed_p = np.array(fixed_p)
-    return np.sum(fixed_p*np.log(fixed_div))
-
+    return np.sum(np.where(p != 0, p * np.log(p / q), 0))

decentralizedFusion.py
@@ -7,12 +7,20 @@ from scipy.stats import multivariate_normal
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
+import cProfile, pstats, io
+from pstats import SortKey
+
 def plot_ellipse(covariance, ax, label_t):
     x_el = np.array([np.sin(np.linspace(0, 2*math.pi, num=63)), np.cos(np.linspace(0, 2*math.pi, num=63))])
     C = np.linalg.cholesky(covariance)
     y_el = np.dot(C, x_el)
     ax.plot(y_el[0], y_el[1], label=label_t)
 
+def plot_last_ellipse(covariance, ax, label_t):
+    x_el = np.array([np.sin(np.linspace(0, 2*math.pi, num=63)), np.cos(np.linspace(0, 2*math.pi, num=63))])
+    C = np.linalg.cholesky(covariance)
+    y_el = np.dot(C, x_el)
+    ax.plot(y_el[0], y_el[1], label=label_t, linewidth=3)
 
 def CI(mu_s, cov_s, weights):
     total_cov = np.zeros(cov_s[0].shape)
@@ -45,7 +53,7 @@ def get_weights(sensor_covariances):
     weights = determinats/np.linalg.norm(determinats)
     return weights, c
 
-def covarianceIntersection(sensor_readings, sensor_covariances, N_time_steps, neighbors, N_agents, KL_inp, true_dist):
+def covarianceIntersection(sensor_readings, sensor_covariances, N_time_steps, neighbors, N_agents, KL_inp, true_dist, calculate_KL=True, calculate_det=True):
     KL_div = {}
     ax = plt.axes()
     determinant_progression = {}
@@ -63,10 +71,12 @@ def covarianceIntersection(sensor_readings, sensor_covariances, N_time_steps, ne
                 mu_s.append(sensor_readings[k])
                 cov_s.append(sensor_covariances[k])
             sensor_readings[j], sensor_covariances[j] = find_optimal_cov_and_mu(5, mu_s, cov_s)
-            dist = multivariate_normal(sensor_readings[j], sensor_covariances[j])
-            KL_div[j].append(compute_KL(true_dist, dist, KL_inp))
-            determinant_progression[j].append(lin.det(sensor_covariances[j]))
-    plot_ellipse(sensor_covariances[0], ax, "FINAL")
+            if(calculate_KL):
+                dist = multivariate_normal(sensor_readings[j], sensor_covariances[j])
+                KL_div[j].append(compute_KL(true_dist, dist, KL_inp))
+            if(calculate_det):
+                determinant_progression[j].append(lin.det(sensor_covariances[j]))
+    plot_last_ellipse(sensor_covariances[0], ax, "FINAL")
     plt.legend(loc='upper left', borderaxespad=0.)
     plt.grid(b = True)
     plt.title("Covariance Ellipses")
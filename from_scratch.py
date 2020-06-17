10e6776283a9cd04fef7c4c74f5f9b5e5a3c09bd
Clark Taylor <clark.n.taylor@gmail.com>
2020-06-17 17:36:02 -0400

    case_1 working in from_scratch.py

--- from_scratch.py
+++ from_scratch.py
@@ -23,8 +23,8 @@ def calculate_value(eigs, q, nu, z, r):
 
     C = (q*(nu*q-2*z))
     D = 2*(1+nu*eigs)
-    print(f'eigs is {eigs}, q is {q}, nu is {nu}, z is {z}')
-    print(f'A is {A}, B is {B}, C is {C}, D is {D}, r is {r}')
+    # print(f'eigs is {eigs}, q is {q}, nu is {nu}, z is {z}')
+    # print(f'A is {A}, B is {B}, C is {C}, D is {D}, r is {r}')
     return np.sum(A/B - C/D) + r
 
 def calculate_derivative(eig, z, q, nu):
@@ -35,23 +35,24 @@ def case_1(bounds, eig, q, z, r):
     vfunc = np.vectorize(calculate_value, excluded=['eigs', 'z', 'q'])
     qfunc = np.vectorize(calculate_derivative, excluded=['eig', 'z', 'q'])
     print(bounds)
-    nus = np.linspace(-100, 100, 10000)
-    vals = np.abs(vfunc(eigs=eig, q=q, nu=nus, z=z, r=r))
+    nus = np.linspace(bounds[0]+1E-3, bounds[1]-1E-3, int((bounds[1]-bounds[0])*1024))
+    vals = vfunc(eigs=eig, q=q, nu=nus, z=z, r=r)
     ders = qfunc(eig=eig, z=z, q=q, nu=nus)
-    print(ders[1])
-    print(ders[2])
-    print(ders[3])
-    print(ders[4])
-    print(ders[5])
-    print(ders[6])
-    print(ders[7])
-    print(ders[8])
-    print(ders[9])
-    print(ders[10])
-    print(ders[11])
-    print(ders[12])
-    print("...")
-    print(ders[-2])
+    # print(ders[1])
+    # print(ders[2])
+    # print(ders[3])
+    # print(ders[4])
+    # print(ders[5])
+    # print(ders[6])
+    # print(ders[7])
+    # print(ders[8])
+    # print(ders[9])
+    # print(ders[10])
+    # print(ders[11])
+    # print(ders[12])
+    # print("...")
+    # print(ders[-2])
+    print('max derivative is',np.max(ders), 'min derivative is',np.min(ders))
     ax = plt.axes()
     ax.plot(nus, vals)
     plt.show()
@@ -61,9 +62,9 @@ def case_1(bounds, eig, q, z, r):
     a = np.abs(ders)
     # print(ders[np.argwhere(a == np.min(a))][0])
     nu = nus[np.argwhere(a == np.min(a))][0]
-    # print(nu)
+    print(nu)
     # print(-0.5*np.linalg.inv(np.identity(2) + nu*np.diag(eig))@(nu*q-2*z))
-    return -0.5*np.linalg.inv(np.identity(2) + nu*np.diag(eig))@(nu*q-2*z)
+    return -0.5*np.linalg.inv(np.identity(len(eig)) + nu*np.diag(eig))@(nu*q-2*z)
     
 
 def case_2(bounds, eig, q, z, r):
@@ -80,12 +81,13 @@ def determine_nu(delta, z, q, r, Q):
     eig = np.diag(delta)
     print('eig is \n',eig)
     bounds = get_bounds_for_nu(eig)
+
     #These next two will be NAN by definition of the bounds...
     val1 = calculate_value(eig, q, bounds[0], z, r)
     val2 = calculate_value(eig, q, bounds[1], z, r)
-    
+
     print('val1 is ',val1,'and val2 is',val2)
-    #x_hat = case_1(bounds, eig, q, z, r)
+    x_hat = case_1(bounds, eig, q, z, r)
     print(Q @ x_hat)
     return Q @ x_hat
     # case_2(bounds, eig, q, z, r)
@@ -102,9 +104,10 @@ def solve_QPQC(z, P, q, r):
 
     return determine_nu(delta_eig, z_hat, q_hat, r, Q)
 
-print("A simple test...")
-z = np.array([1.,0.])
-P = np.array([[1,0.],[0.,-.5]])
-q = np.zeros((2,))
-r = 0
-solve_QPQC(z,P,q,r)
+if __name__ == "__main__":
+    print("A simple test...")
+    z = np.array([1.,0.])
+    P = np.array([[2,0.],[0.,-1]])
+    q = np.array([1.,0])
+    r = 0
+    solve_QPQC(z,P,q,r)

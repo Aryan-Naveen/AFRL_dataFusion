1fcdeacddc7419fa29f7d9d54d3ad30059e08ff4
Clark Taylor <clark.n.taylor@gmail.com>
2020-06-17 17:24:21 -0400

    Debugging from_scratch.py

--- from_scratch.py
+++ from_scratch.py
@@ -5,19 +5,27 @@ import matplotlib.pyplot as plt
 
 def get_bounds_for_nu(e):
     bounds = np.zeros((2, 1))
+    eig_min = np.min(e)
+    eig_max = np.max(e)
+    if eig_min>=0:
+        print("WARNING:  There is no upper limit for nu. It can go to +\infty")
+    if eig_max <= 0:
+        print("WARNING:  nu will have to be negative for I+nu \Lambda to be psd. No limits on it")
     bounds[1]= -1/np.min(e)
     bounds[0] = -1/np.max(e)
-    # print(np.sort(bounds))
+    print('bounds before sort',bounds,'and after sort',np.sort(bounds))
     return np.sort(bounds)
     
 
 def calculate_value(eigs, q, nu, z, r):
-    A = np.sum(eigs*np.power(nu*q-2*z, 2))
-    B = np.sum(4*np.power(1+nu*eigs, 2))
+    A = eigs*np.power(nu*q-2*z, 2)
+    B = 4*np.power(1+nu*eigs, 2)
 
-    C = np.sum(2*(1+nu*eigs))
-    D = np.sum((q*(nu*q-2*z)))
-    return A/B + C/D + r
+    C = (q*(nu*q-2*z))
+    D = 2*(1+nu*eigs)
+    print(f'eigs is {eigs}, q is {q}, nu is {nu}, z is {z}')
+    print(f'A is {A}, B is {B}, C is {C}, D is {D}, r is {r}')
+    return np.sum(A/B - C/D) + r
 
 def calculate_derivative(eig, z, q, nu):
     return -np.sum(np.power(2*eig*z+q, 2)/np.power(2*(1+nu*eig), 3))
@@ -70,10 +78,14 @@ def case_2(bounds, eig, q, z, r):
 def determine_nu(delta, z, q, r, Q):
     pot_nus = np.linspace(-1000, 1000, 2000)
     eig = np.diag(delta)
+    print('eig is \n',eig)
     bounds = get_bounds_for_nu(eig)
+    #These next two will be NAN by definition of the bounds...
     val1 = calculate_value(eig, q, bounds[0], z, r)
     val2 = calculate_value(eig, q, bounds[1], z, r)
-    x_hat = case_1(bounds, eig, q, z, r)
+    
+    print('val1 is ',val1,'and val2 is',val2)
+    #x_hat = case_1(bounds, eig, q, z, r)
     print(Q @ x_hat)
     return Q @ x_hat
     # case_2(bounds, eig, q, z, r)
@@ -89,3 +101,10 @@ def solve_QPQC(z, P, q, r):
     z_hat = Q.T @ z
 
     return determine_nu(delta_eig, z_hat, q_hat, r, Q)
+
+print("A simple test...")
+z = np.array([1.,0.])
+P = np.array([[1,0.],[0.,-.5]])
+q = np.zeros((2,))
+r = 0
+solve_QPQC(z,P,q,r)
